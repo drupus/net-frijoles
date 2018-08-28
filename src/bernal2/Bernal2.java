ec2749976d2be1bb2ccbcc60602884afd11ef99f
Alexis Hernandez Martinez <alexish@live.com.mx>
2018-08-28 17:38:24 -0500

    que tranza

--- src/bernal2/Bernal2.java
+++ src/bernal2/Bernal2.java
@@ -15,7 +15,7 @@ public class Bernal2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println ("que onda que pez");
+        System.out.println ("Que tranza karnal");
     }
     
 }
