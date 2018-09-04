94c2001cd492c85cfa3800a903d473184ca04d8e
Alexis Hernandez Martinez <alexish@live.com.mx>
2018-09-04 16:01:40 -0500

    agregué la funcion de multiplicacion

--- src/bernal2/Bernal2.java
+++ src/bernal2/Bernal2.java
@@ -35,4 +35,16 @@ public class Bernal2 {
     double suma;  
     }
 
+      public class Multiplicacion extends Operacion{
+    
+    double multi;
+       
+    public Multiplicacion(double n1, double n2) {
+             
+        super(n1, n2, '*');
+        this.multi = n1 * n2;
+        this.setRes(this.multi);
+    }
+} 
+       
 }
